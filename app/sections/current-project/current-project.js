746ead7c8418b75439c0cc42e31d84960628f549
RoldanIsaac <orlandollamos10@gmail.com>
2025-05-19 18:42:20 -0400

    Project cards implementation

--- app/sections/current-project/current-project.js
+++ app/sections/current-project/current-project.js
@@ -11,30 +11,28 @@ export class CurrentProject extends Component {
       styles: "app/sections/current-project/current-project.css",
       imports: [...chips],
       script: () => {
-        const card = {
-          url: "app/components/project-card/project-card.html",
-          destinationId: "projects__cards-container",
-        };
-
-        const cardData = [
-          {
-            url: "public/screenshoots/sho-2.png",
-            title: "SHO Management System",
-            description: "Descripción de la Card 1",
-          },
-          {
-            url: "public/screenshoots/sho-3.png",
-            title: "Pro Dynamic Web",
-            description: "Descripción de la Card 3",
-          },
-          {
-            url: "public/screenshoots/sho.png",
-            title: "Google Services Integration",
-            description: "Descripción de la Card 2",
-          },
-        ];
-
-        domInjector(card.url, card.destinationId, cardData);
+        // const card = {
+        //   url: "app/components/project-card/project-card.html",
+        //   destinationId: "projects__cards-container",
+        // };
+        // const cardData = [
+        //   {
+        //     url: "public/screenshoots/sho-2.png",
+        //     title: "SHO Management System",
+        //     description: "Descripción de la Card 1",
+        //   },
+        //   {
+        //     url: "public/screenshoots/sho-3.png",
+        //     title: "Pro Dynamic Web",
+        //     description: "Descripción de la Card 3",
+        //   },
+        //   {
+        //     url: "public/screenshoots/sho.png",
+        //     title: "Google Services Integration",
+        //     description: "Descripción de la Card 2",
+        //   },
+        // ];
+        // domInjector(card.url, card.destinationId, cardData);
       },
     });
   }
